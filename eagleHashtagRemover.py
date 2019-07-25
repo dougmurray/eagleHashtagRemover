a674a4d10a7ab3be13d94b453e402551b29e9b3c
Douglass Murray <douglass.murray@gmail.com>
2019-07-25 16:12:18 -0400

    Added python script which finds then removes files with the extension .b#number, like pcb.b#1.

---
+++ eagleHashtagRemover.py
@@ -0,0 +1,53 @@
+# eagleHashtagRemover.py
+#! /usr/bin/env python 3
+"""Script to remove all CADSoft EAGLE .b# unnecessary files.
+
+	Simply run: eagleHastagRemover.py /directory/to/search/
+
+	Does 2 things: first finds all .b# files in the directory, 
+	and second removes them.
+"""
+import os
+import sys
+import fnmatch
+
+def hastag_finder(searchDirectory):
+	"""Finds location of all CADSoft EAGLE .b# files.
+
+	Args:
+		searchDirectory: Directory to search for .b# files.
+
+	Returns:
+		filesToRemove: List of locations of .b# files.
+	"""
+	filesToRemove = []
+	for file in os.listdir(searchDirectory):
+		# Look for all files in directory that match filenames *b#*, ex Eric.b#3
+		if fnmatch.fnmatch(file, '*.b#*'):
+			fullLocation = searchDirectory + '/' + file
+			filesToRemove.append(fullLocation)
+
+	return filesToRemove
+
+def hashtag_remover(fileList):
+	"""Deletes all found CADSoft EAGLE .b# files.
+
+	Args:
+		fileList: Directory location where the .b# files are in.
+
+	Returns:
+		Nothing.
+	"""
+	for file in fileList:
+		print("Trying to delete: ", file)
+		try:
+			os.remove(file)
+		except:
+			print("Error while deleting file : ", file)
+
+def main():
+	hashtag_files = hastag_finder(sys.argv[1])
+	hashtag_remover(hashtag_files)
+
+if __name__ == '__main__':
+	main()
